5b8674b024c11b7c2596794c16581e16e6cc34ad
Danielle <136658130+Smashhedo@users.noreply.github.com>
2023-12-11 21:37:50 -0600

    Added queries

--- queries.sql
+++ queries.sql
@@ -1,22 +1,23 @@
 --Part 1
 
-CREATE TABLE job (
-   id INT,
-   name VARCHAR(100),
-   location VARCHAR(100),
-);
+/*
+id int
+name varchar
+*/
 
 --Part 2
 
-INSERT INTO employer (id, name, location)
-VALUES (1, "Wells Fargo", "St. Louis City");
-
 SELECT name
 FROM employer
-WHERE (location = "St. Louis City");
+WHERE location IN ('St. Louis City');
 
 --Part 3
 
-
+DELETE FROM job;
 
 --Part 4
+
+SELECT COUNT (employerId), name
+FROM skills
+WHERE EXISTS skills
+ORDER BY VARCHAR name;
